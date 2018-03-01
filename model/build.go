7fd6aa0a3a1d7c6c14847558cfe10151abfe8f47
Sergey Dzyuban <sergey.d@sbtech.com>
2018-03-01 16:38:10 +0200

    event added

---
+++ model/build.go
@@ -0,0 +1,14 @@
+package build
+
+// Build for Jenkins Job Report
+type Build struct {
+	JobID          int64  `json:"jobid"`
+	Job            string `json:"job"`
+	RepositoryURL  string `json:"repositoryurl"`
+	RepositoryName string `json:"repositoryname"`
+	BranchName     string `json:"branchname"`
+	Status         string `json:"status"`
+	Slave          string `json:"slave"`
+	Author         string `json:"author"`
+	Duration       int64  `json:"duration"`
+}
